53c33b860af46bd130989a330da464b54a6f46e0
Noah Stephen Haskell <nstephenh@gmail.com>
2015-12-10 14:12:09 -0500

    Javadoc, test code, fixed equals

--- Complex.java
+++ Complex.java
@@ -1,48 +1,13 @@
 /**
  * @author Noah Stephen Haskell
  *
- *
+ *<br>
  * This is a class of immutable complex numbers capable of all 
  * features of complex arithmetic.
  *
- *It features the following constants:
- * <ul>
- *<li><code>Complex.ONE</code>  This is 1 + 0i.</li>
-*<li><code>Complex.ZERO</code>  This is 0 + 0i.</li>
-*<li><code>Complex.I</code>  This is  0 +  i.</li>
-*</ul>
- * And the following methods/operations
-*<ul>
-*<li><code>Complex(double _real, double _imaginary)</code>  This
-*constructor creates <code>_real + i*_imaginary</code></li>
-*<li><code>Complex(double _real)</code>  This
-*constructor creates <code>_real + i*0</code></li>
-*<li><code>Complex()</code>  This
-*constructor creates <code>0 + i*0</code></li>
-*<li><code>String toString()</code> returns a string representation that
-*looks like <code>a + ib</code> or <code>a - ib</code>.</li>
-*<li><code>boolean equals(Object o)</code> returns <code>true</code>
-*if <code>o</code> is a <code>Complex</code> and if its real and imaginary
-*parts agree with those of this complex number.</li>
-*looks like <code>a + ib</code> or <code>a - ib</code>.</li>
-*<li><code>Complex add(Complex that)</code> Returns <code>this + that</code></li>
-*<li><code>Complex subtract(Complex that)</code>returns <code>this - that</code></li>
-*<li><code>Complex multiply(Complex that)</code>returns <code>this * that</code></li>
-*<li><code>Complex divide(Complex that)</code>returns <code>this / that</code></li>
-*<li><code>Complex conjugate()</code> returns the conjugate of <code>this </code></li>
-*<li><code>Complex magnitude()</code> returns <code>|this|</code>. In mathematical parlance, we use \(|z|\) for the
-magnitude of a complex number. Notice how this is an
-extension of the definition of absolute value for a real
-number. </li>
-*<li><code>Complex pow(int n)</code> returns <code>this<sup>n</sup></code></li>
-*<li><code>double re()</code> returns the real part of <code>this</code></li>
-*<li><code>double im()</code> returns the imaginary part of <code>this</code></li>
-*<li><code>double arg()</code> returns the argument of <code>this</code>. For a complex number \(z = a + ib\), its <em>argument</em>
-is defined as \(\arcsin(b/|z|)\). This it the angle it makes,
-as a vector, with the positive real (\(x\)-axis) in the complex
-plane.</li>
-*</ul>
+ *
  */
+
 public class Complex
 {
     private static final double TINY = 1e-6;
@@ -53,7 +18,9 @@ public class Complex
     public static Complex ZERO = new Complex();
     public static Complex I = new Complex(0,1);
 
-
+    /**
+    *   Constructs a complex in the format a+bi, where a is the real term and i is the imaginary term
+     */
     public Complex(double _real, double _imaginary)
     {
         //a + bi, where real is a and imaginary is b
@@ -61,16 +28,23 @@ public class Complex
         this.b = _imaginary;
     }
     
+    /**
+     * Constructs a complex with no imaginary term.
+     */
     public Complex(double _real)
     {
         this.a = _real;
         this.b = 0;
     }
+    /** Constructs a complex with no imaginary or real term.
+     */
     public Complex()
     {
         this.a = 0;
         this.b = 0;
     }
+    /** Prints the construct in the form a+bi, replacing + with - if b is negative
+     */
     @Override
     public String toString(){
         String printme;
@@ -88,60 +62,89 @@ public class Complex
     {
         return z.subtract(w).magnitude() < TINY;
     }
+    /** Checks to see if object o is a complex, and if it is, then checks to see if a == a and b == b
+     */
     @Override
     public boolean equals(Object o) {
-        if (o.getClass().equals(ZERO)){
-            Complex cmplx = (Complex)o;
+        if (o instanceof Complex){
+            Complex cmplx = (Complex) o;
+            System.out.println(cmplx + " " + this);
             if ((cmplx.a == this.a) && (cmplx.b == this.b)){
                 return true;
             }
         }
         return false;
+        
     }
+    /** Adds two complex by combining like terms
+      */
     public Complex add(Complex that) {
         return new Complex((this.a + that.a), (this.b + that.b));
     }
+    /** Subtracts two complex numbers by combining like terms
+      */
     public Complex subtract(Complex that) {
         return new Complex((this.a - that.a), (this.b - that.b));
     }
+    /** Multiplies two Complexes (a+bi)(c+di)=(a*c)-(b-d)+i*(a*d)+(b*c)
+      */
     public Complex multiply(Complex that) {
         return new Complex(((this.a*that.a)-(this.b*that.b)),((this.a*that.b)+(this.b*that.a)));
     }
+    /** Divides two complexes by dividing the product of two complexes (a+bi)(c+di) by c squared plus d squared
+     */
     public Complex divide(Complex that) {
         Complex numerator = this.multiply(that.conjugate());
         Double denominator = (that.magnitude()*that.magnitude());
         return new Complex(numerator.a/denominator, numerator.b/denominator);
     }
+    /** returns the congugate of the complex
+      */
     public Complex conjugate() {
         return new Complex(this.a, 0 - this.b);
     }
+    /** return the absolute value of the complex
+      */
     public double magnitude() {
         return Math.sqrt((this.a*this.a)+(this.b*this.b));
     }
+    /** return the complex to the n. n is an integer
+      */
     public Complex pow(int n) {
         Complex total = ONE;
         int count = 1;
         if(n < 0 ){
+          n = 0-n;
             do{
                 total = total.divide(this);
-            } while (count != n);
+                count ++;
+            } while (count != n+1);
         }
         else {
             do {
                 total = total.multiply(this);
-            } while (count != n);
+                count ++;
+            } while (count != n+1);
         }
         return total;
     }
+    /** return the real term of the complex
+      */
     public double re() {
         return this.a;
     }
+    /** return the imaginary term of the complex
+      */
     public double im() {
         return this.b;
     }
+    /** return the argument of the complex
+      */
     public double arg() {
         return Math.asin(this.b/this.magnitude());
     }
+    /** This is test code
+      */
     public static void main(String[] args)
     {
         Complex test1 = new Complex(3, 5);
@@ -158,13 +161,16 @@ public class Complex
         System.out.println(closeEnough(new Complex(-9, 19).divide(test1), test2));
         System.out.println("toString testing:");
         System.out.println(test1 + " " + test2 + " " + new Complex(9,-19));
+        System.out.println("equals testing:");
+        System.out.println(test1.equals(test1));
         System.out.println("Congutate testing:");
-        System.out.println(closeEnough(test1, new Complex(3, -5)));
+        System.out.println(closeEnough(test1.conjugate(), new Complex(3, -5)));
         System.out.println(closeEnough(test1.conjugate().conjugate(), test1));
         System.out.println("Magnitude testing:");
         System.out.println(test2.magnitude() == Math.sqrt(13));
         System.out.println("Power testing");
-        System.out.println();
+        System.out.println(closeEnough(new Complex(3,2).pow(2), new Complex(5, 12)));
+        System.out.println(closeEnough(new Complex(2).pow(-1), new Complex(.5)));
         System.out.println("Re testing:");
         System.out.println(test1.a == test1.re());
         System.out.println("im testing:");
